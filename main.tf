1ecd9c69e1782b3dfd1bec3c80c88310e5b65529
Gaveen_Buddhika <buddhik-se20022@stu.kln.ac.lk>
2024-11-19 17:12:15 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# vpc module
 module "vpc" {
   source = "./modules/vpc"
   cidr   = "10.0.0.0/16"  #Replace with your custom cidr
 }
 
+#subnet module
 module "subnet" {
   source            = "./modules/subnet"
   vpc_id            = module.vpc.vpc_id
@@ -14,15 +16,31 @@ module "subnet" {
   availability_zone = "us-east-1a"
 }
 
+
+# Internet Gateway Module
+module "internet_gateway" {
+  source   = "./modules/internet_gateway"
+  vpc_id   = aws_vpc.main_vpc.id
+  igw_name = "Main-IGW"
+}
+
+# Route Table Module
+module "route_table" {
+  source    = "./modules/route_table"
+  vpc_id    = aws_vpc.main_vpc.id
+  gateway_id = module.internet_gateway.internet_gateway_id
+  subnet_id = aws_subnet.main_subnet.id
+  rt_name   = "Main-Route-Table"
+}
+
+#security group module
 module "security_group" {
   source = "./modules/security_group"
   name   = "web"
   vpc_id = module.vpc.vpc_id
 }
 
-
-
-
+#ec2 instance module
 module "ec2_instance" {
   source            = "./modules/ec2_instance"
   ami_id            = "ami-053b0d53c279acc90"   #Replace with your  ami
--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 resource "aws_instance" "Example" {
   ami = var.ami_id
   instance_type = var.instance_type
+  vpc_security_group_ids = [var.security_group_id]
+  subnet_id = var.subnet_id
 
   tags = {
     Name = "ExampleInstance"
